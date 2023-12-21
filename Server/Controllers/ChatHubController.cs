cd067f4cd42720982cb6e91c8deffa30022ddf2b
nguyenhoangan <nnguyenhoangann@gmail.com>
2023-12-21 16:12:03 +0700

    optimize code before and add msg controller

--- Server/Controllers/ChatHubController.cs
+++ Server/Controllers/ChatHubController.cs
@@ -22,45 +22,71 @@ namespace Server.Controllers
             _authLibrary = authLibrary;
         }
 
+        private async Task<User?> GetUserFromAccessToken()
+        {
+            // Get the access token from the authorization header
+            string? accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                BadRequest("Access token is required");
+                return null;
+            }
+
+            var principal = _authLibrary.Validate(accessToken);
+
+            if (principal == null)
+            {
+                BadRequest("Invalid access token");
+                return null;
+            }
+
+            // Get the user's name from the access token claims
+            string? userName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                BadRequest("Invalid username");
+                return null;
+            }
+
+            // Get the user from the database by name
+            User? userFromDb = await _db.Users.FirstOrDefaultAsync(u => u.Username == userName);
+
+            if (userFromDb == null)
+            {
+                NotFound("User not found");
+                return null;
+            }
+
+            return userFromDb;
+        }
+
         [HttpPost("saveSignalRId")]
         public async Task<IActionResult> SaveSignalRId([FromBody] SaveSignalRIdRequest saveSignalRIdRequest)
         {
-            var sId = saveSignalRIdRequest.SId;
-            // Get the access token from the authorization header
-            string? accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            if (string.IsNullOrEmpty(accessToken))
+            string sId = saveSignalRIdRequest.SId;
+            if (string.IsNullOrEmpty(sId))
             {
-                return BadRequest("Access token is required");
+                return BadRequest();
             }
-            var principal = _authLibrary.Validate(accessToken);
-            if (principal == null)
+            User? userFromDb = await GetUserFromAccessToken();
+            if (userFromDb == null)
             {
-                return BadRequest("Invalid access token");
-            }
-            // Get the user's name from the access token claims
-            string? userName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
-            if (!string.IsNullOrEmpty(sId) && !string.IsNullOrEmpty(userName))
-            {
-                User? userFromDb = await _db.Users.Where(u => u.Username == userName).FirstOrDefaultAsync();
-                if (userFromDb == null)
-                {
-                    return NotFound("User not found");
-                }
-
-                SignalRConnectionId newRecord = new()
-                {
-                    UserId = userFromDb.Id,
-                    Value = saveSignalRIdRequest.SId,
-                    CreationTime = DateTime.Now
-                };
-
-                _db.SignalRConnectionIds.Add(newRecord);
-                await _db.SaveChangesAsync();
-
-                return Ok();
+                return NotFound("User not found");
             }
 
-            return BadRequest();
+            SignalRConnectionId newRecord = new()
+            {
+                UserId = userFromDb.Id,
+                Value = saveSignalRIdRequest.SId,
+                CreationTime = DateTime.Now
+            };
+
+            _db.SignalRConnectionIds.Add(newRecord);
+            await _db.SaveChangesAsync();
+
+            return Ok();
         }
 
         [HttpGet("getSignalRId")]
@@ -94,60 +120,23 @@ namespace Server.Controllers
         [HttpGet("getAllDuoConversationInfo")]
         public async Task<IActionResult> GetAllDuoConversationInfo()
         {
-            // Get the access token from the authorization header
-            string? accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            if (string.IsNullOrEmpty(accessToken))
-            {
-                return BadRequest("Access token is required");
-            }
-            var principal = _authLibrary.Validate(accessToken);
-            if (principal == null)
-            {
-                return BadRequest("Invalid access token");
-            }
-            // Get the user's name from the access token claims
-            string? userName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
-            // Check if the user's name is null or empty
-            if (string.IsNullOrEmpty(userName))
-            {
-                return BadRequest("Invalid username");
-            }
-            // Get the user from the database by name
-            User? userFromDb = await _db.Users.FirstOrDefaultAsync(u => u.Username == userName);
-            // Check if the user exists
+            User? userFromDb = await GetUserFromAccessToken();
             if (userFromDb == null)
             {
                 return NotFound("User not found");
             }
 
-            List<GetAllDuoConversationInfoReturn>? list = new();
-            List<User>? userList = new();
-            userList = await _db.Users.ToListAsync();
-
-            if (userList.Count > 0)
-            {
-                foreach (var user in userList)
+            List<GetAllDuoConversationInfoReturn> list = await (
+                from user in _db.Users
+                where user.Id != userFromDb.Id
+                join signalRConnectionId in _db.SignalRConnectionIds on user.Id equals signalRConnectionId.UserId into signalRIds
+                from s in signalRIds.OrderByDescending(s => s.CreationTime).Take(1).DefaultIfEmpty()
+                select new GetAllDuoConversationInfoReturn
                 {
-                    if (user != userFromDb)
-                    {
-                        SignalRConnectionId? signalRConnectionId = await _db.SignalRConnectionIds
-                            .Where(s => s.UserId == user.Id)
-                            .OrderByDescending(s => s.CreationTime)
-                            .FirstOrDefaultAsync();
-
-                        if (signalRConnectionId != null)
-                        {
-                            GetAllDuoConversationInfoReturn temp = new()
-                            {
-                                Username = user.Username,
-                                SignalRId = signalRConnectionId.Value
-                            };
-
-                            list.Add(temp);
-                        }
-                    }
+                    Username = user.Username,
+                    SignalRId = s.Value
                 }
-            }
+            ).ToListAsync();
 
             return Ok(list);
         }
@@ -155,59 +144,25 @@ namespace Server.Controllers
         [HttpGet("getAllGroupConversationInfo")]
         public async Task<IActionResult> GetAllGroupConversationInfo()
         {
-            // Get the access token from the authorization header
-            string? accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            if (string.IsNullOrEmpty(accessToken))
+            User? userFromDb = await GetUserFromAccessToken();
+            if (userFromDb == null)
             {
-                return BadRequest("Access token is required");
+                return NotFound("User not found");
             }
-            var principal = _authLibrary.Validate(accessToken);
-            if (principal == null)
-            {
-                return BadRequest("Invalid access token");
-            }
-            // Get the user's name from the access token claims
-            string? userName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
-            if (!string.IsNullOrEmpty(userName))
-            {
-                User? user = await _db.Users.Where(u => u.Username == userName).FirstOrDefaultAsync();
-                if (user != null)
+
+            List<GetAllGroupConversationInfoReturn> conversationsReturn = await (
+                from participant in _db.Participants
+                where participant.UserId == userFromDb.Id
+                join conversation in _db.Conversations on participant.ConversationId equals conversation.Id
+                where conversation.ConversationType == "group"
+                select new GetAllGroupConversationInfoReturn
                 {
-                    List<GetAllGroupConversationInfoReturn>? list = new();
-                    List<Participant> participants = await _db.Participants.Where(p => p.UserId == user.Id).ToListAsync();
-                    List<GetAllGroupConversationInfoReturn> conversationsReturn = new();
-
-                    if (participants.Count > 0)
-                    {
-                        List<Conversation> conversations = new();
-
-                        foreach (var item in participants)
-                        {
-                            conversations.Add(await _db.Conversations.Where(c => c.Id == item.ConversationId).FirstAsync());
-                        }
-
-                        if (conversations.Count > 0)
-                        {
-
-                            foreach (var item in conversations)
-                            {
-                                if (item.ConversationType == "group")
-                                {
-                                    conversationsReturn.Add(new GetAllGroupConversationInfoReturn
-                                    {
-                                        GroupId = item.Id,
-                                        GroupName = item.ConversationName
-                                    });
-                                }
-                            }
-                        }
-                    }
-
-                    return Ok(conversationsReturn);
+                    GroupId = conversation.Id,
+                    GroupName = conversation.ConversationName
                 }
-            }
+            ).ToListAsync();
 
-            return BadRequest();
+            return Ok(conversationsReturn);
         }
     }
 
---
+++ Server/Controllers/MessageController.cs
@@ -0,0 +1,124 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Auth;
+using Server.Helpers;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/message")]
+    public class MessageController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly AuthLibrary _authLibrary;
+
+        public MessageController(ApplicationDbContext db, AuthLibrary authLibrary)
+        {
+            _db = db;
+            _authLibrary = authLibrary;
+        }
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllMessages()
+        {
+            // Get the access token from the authorization header
+            string? accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                return BadRequest("Access token is required");
+            }
+            var principal = _authLibrary.Validate(accessToken);
+            if (principal == null)
+            {
+                return BadRequest("Invalid access token");
+            }
+            // Get the user's name from the access token claims
+            string? userName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            if (string.IsNullOrEmpty(userName))
+            {
+                return BadRequest("Invalid username");
+            }
+            User? userFromDb = await _db.Users.Where(u => u.Username == userName).FirstOrDefaultAsync();
+            // Check if the user exists
+
+            if (userFromDb == null)
+            {
+                return BadRequest();
+            }
+
+            var conversations = await _db.Participants
+                .Where(p => p.UserId == userFromDb.Id)
+                .Join(_db.Conversations, p => p.ConversationId, c => c.Id, (p, c) => c)
+                .ToListAsync();
+
+            var returnList = new List<GetAllMessagesReturn>();
+
+            foreach (var conversation in conversations)
+            {
+                var messagesDTO = await _db.Messages
+                    .Where(m => m.ConversationId == conversation.Id)
+                    .Select(m => new MessageDTO
+                    {
+                        SenderId = m.SenderId,
+                        Content = m.Content,
+                        SendAt = m.SentAt,
+                        MessageType = m.MessageType
+                    })
+                    .ToListAsync();
+
+                if (conversation != null)
+                {
+                    string? conversationName = conversation.ConversationType == "group"
+                        ? conversation.ConversationName ?? conversation.Id.ToString()
+                        : await _db.Participants
+                            .Where(p => p.ConversationId == conversation.Id && p.UserId != userFromDb.Id)
+                            .Join(_db.Users, p => p.UserId, u => u.Id, (p, u) => u.Username)
+                            .FirstOrDefaultAsync();
+
+                    if (!string.IsNullOrEmpty(conversationName))
+                    {
+                        var temp = new GetAllMessagesReturn
+                        {
+                            ConversationId = conversation.Id.ToString(),
+                            ConversationName = conversationName,
+                            ConversationType = conversation.ConversationType,
+                            MessagesDTO = messagesDTO
+                        };
+                        returnList.Add(temp);
+                    }
+                }
+            }
+
+            // var options = new JsonSerializerOptions
+            // {
+            //     ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            //     MaxDepth = 64,
+            // };
+
+            // string jsonString = JsonSerializer.Serialize(returnList, options);
+
+            return Ok(returnList);
+        }
+
+    }
+
+    public class MessageDTO
+    {
+        public required long SenderId { get; set; }
+        public required string Content { get; set; }
+        public required DateTime SendAt { get; set; }
+        public required string MessageType { get; set; }
+    }
+
+    public class GetAllMessagesReturn
+    {
+        public required string ConversationId { get; set; }
+        public required string ConversationName { get; set; }
+        public required string ConversationType { get; set; }
+        public required List<MessageDTO>? MessagesDTO { get; set; }
+    }
+}
